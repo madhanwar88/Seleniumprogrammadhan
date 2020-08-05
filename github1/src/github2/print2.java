8f5fa344ce10434a09f4e76e7a5b9a80b1a4eab0
intel <intel@intel-PC>
2020-08-05 09:45:58 +0530

    New Sys

---
+++ github1/src/github2/print2.java
@@ -0,0 +1,11 @@
+package github2;
+
+public class print2 {
+
+	public static void main(String[] args) {
+		System.out.println("Sujan Master");
+		System.out.println("Sujan Trainer");
+		System.out.println("Trainee Madhan and Karthikgee");
+
+	}
+}
